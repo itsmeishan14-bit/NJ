11b5fe9c255cc188eff977ff4bb80c1ed61ed232
Ishan Timalsina <itsmeishan14@gmail.com>
2026-09-05 02:48:39 +0530

    calculator

---
+++ tictacto/script.js
@@ -0,0 +1,77 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn")
+let msgContainer=document.querySelector(".msg-container")
+let msg = document.querySelector(".msg");
+
+ let turnO = true;                     //playerX,  playerY
+
+ const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+
+    [3,4,5],
+    [6,7,8],
+ ];
+ const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainerclassList.add("hide");
+
+ };
+const disableBoxes=()=>{
+    for (let box of boxes){
+        box.disabled=true;
+    }
+};
+const enableBoxesBoxes=()=>{
+    for (let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+
+boxes.forEach((box) =>{
+box.addEventListener("click",() =>{
+    console.log("box was clicked");
+    if(turnO){
+box.innerText = "O"
+turnO=false;
+    }
+    else{
+        box.innerText = "X";
+        turnO =true;
+    }
+    box.disabled =true;
+
+    checkWinner();
+});
+});
+const showWinner=(winner)=>{
+    msg.innerText=`conguralation, winner is${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner=()=>{
+ for ( let pattern of  winPatterns){
+    let p1=boxes[pattern[0]].innerText;
+    let p2=boxes[pattern[1]].innerText;
+    let p3=boxes[pattern[2]].innerText;
+    if(p1!=""&&p2!=""&&p3!=""){
+        if(p1===p2 &&p2===p3){
+            console.log("winner",p1)
+            showWinner(p1);
+
+        }
+    }
+ }
+};
+newGameBtn.addEventListener("Click",resetGame)
+resetBtn.addEventListener("Click",resetGame)
